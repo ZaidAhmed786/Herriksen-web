ed5c2032dd8c9f94882abade456ceb4a78f10d9d
Abdur Rafay <158170390+Rafaykz@users.noreply.github.com>
2025-10-17 21:58:59 +0500

    add opppop

---
+++ script.js
@@ -0,0 +1,8 @@
+   function openSealPopup() {
+      // Yeh popup RapidScanSecure ke official seal format me open karega
+      window.open(
+        'https://www.rapidscansecure.com/siteseal/Verify.aspx?code=17,3C133811720FCCB163552D13CC00BA650A6AFA',
+        'SecureSiteSeal',
+        'width=600,height=400,left=100,top=100,resizable=yes,scrollbars=no,status=no'
+      );
+    }
