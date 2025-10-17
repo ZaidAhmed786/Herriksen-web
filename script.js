486786a21c638e88fac6c0ad05d09fa5fb1d2d01
Abdur Rafay <158170390+Rafaykz@users.noreply.github.com>
2025-10-17 22:34:03 +0500

    update done all

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
    function openSealPopup() {
       // Yeh popup RapidScanSecure ke official seal format me open karega
       window.open(
-        'https://www.rapidscansecure.com/siteseal/Verify.aspx?code=17,3C133811720FCCB163552D13CC00BA650A6AFA',
+        'https:https://www.rapidscansecure.com/siteseal/siteseal.js?code=17,3C133811720FCCB163552D13CC00BA650A6AFA5B%27',
         'SecureSiteSeal',
         'width=600,height=400,left=100,top=100,resizable=yes,scrollbars=no,status=no'
       );
